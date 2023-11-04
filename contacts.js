83a0b6570a4b0406a1724258bca4f0f0bb4313f5
Paweł Witecki-Wawrzyniak <pawel.witecki.wawrzyniak@gmail.com>
2023-11-04 20:32:29 +0100

    npm installed

---
+++ contacts.js
@@ -0,0 +1,20 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", contacts.json);
+
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
